9d8cbb175a8842b7cad7505c85484c8fc903d5fd
Tomas <tomaslonnblad@gmail.com>
2025-10-20 07:18:08 +0200

    Adjust spotlight position in Computers component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -18,7 +18,7 @@ const Computers = ({ isMobile }) => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
-        position={[-20, 80, 10]}
+        position={[-20, 90, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
