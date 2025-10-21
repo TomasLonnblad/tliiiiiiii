298ce8190b94a0d2c4b466e55152a428ee9f9d9b
Tomas <tomaslonnblad@gmail.com>
2025-10-21 17:51:38 +0200

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,7 +7,7 @@ import CanvasLoader from "../Loader";
 
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("/desktop_pc/scene.gltf");
+  const computer = useGLTF("https://github.com/TomasLonnblad/tliiiiiiii/tree/main/public/desktop_pc/scene.gltf");
 
   return (
     <mesh>
