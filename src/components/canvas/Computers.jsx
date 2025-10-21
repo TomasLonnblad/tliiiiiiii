ee983ec765196ae35f36cbe6c5ecd5e58bdb7419
Tomas <tomaslonnblad@gmail.com>
2025-10-21 14:33:25 +0200

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -65,7 +65,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [0.1, 0.1, 0.1], fov: 100 }}
+      camera={{ position: [0.1, 0.1, 0.1], fov: 80 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
