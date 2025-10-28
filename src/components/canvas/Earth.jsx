8707f35f2d68ccc2d194131dce6e618f8a6c08c2
Tomas <tomaslonnblad@gmail.com>
2025-10-28 14:37:55 +0100

    Update Earth.jsx

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("/planet/scene.gltf");
+  const earth = useGLTF("/planet/ok3.glb");
 
   return (
     <primitive object={earth.scene} scale={6.5} position-y={0} rotation-y={2} />
